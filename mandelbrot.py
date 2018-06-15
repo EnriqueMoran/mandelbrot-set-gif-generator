941bb4cb05e521e8614471856286b18589c158a9
Enrique Moran <enriquemoran95@gmail.com>
2018-06-16 01:01:30 +0200

    Version 0.36

--- mandelbrot.py
+++ mandelbrot.py
@@ -9,16 +9,17 @@ class Mandelbrot():
 
     __author__: "EnriqueMoran"
 
+    __version__: "0.36"
+
     def transformInterval(self, e1, e2, s1, s2, x):    # Transform [0, image_width] and [0, image_heigth] into [-2, 2] interval
         return (s1 - s2) / (e1 - e2) * (x - e1) + s1
 
-    def __init__(self, image_width, image_heigth, image_path, iteration_depth = 100, zoom = 1, x0 = 0, y0 = 0, c = 0j):
+    def __init__(self, image_width, image_heigth, image_path, iteration_depth = 100, zoom = 1, x0 = 0, y0 = 0):
         self.image_width = image_width
         self.image_heigth = image_heigth
         self.iteration_depth = iteration_depth    # Used on escape time algorithm
         self.image_path = image_path    # Where the resultant image will be saved
         self.zoom = zoom
-        self.c = c    # C on z = z^2 + c equation
         self.s1_x = x0 + 2 * -zoom  # Initial x coordinate
         self.s2_x = x0 + 2 * zoom
         self.s2_y = y0 + 2 * zoom  # Initial y coordinate
@@ -38,26 +39,20 @@ class Mandelbrot():
                 val_j = self.transformInterval(0, self.image_heigth, self.s1_y, self.s2_y, j)
                 value = self.mandelbrot(complex(val_i, val_j))
                 if value == self.iteration_depth:
-                    self.pixels[i, j] = (0, 0 ,0)
+                    self.pixels[i, j] = (0, 0, 0)
                 else:
-                    blue, green, red = intToRGB(value)
-                    self.pixels[i, j] = (blue, green, red)
+                    red, green, blue = intToRGB(value)
+                    self.pixels[i, j] = (red, 0, 0)
         if save:
             self.img.save(self.image_path)    # Save every frame
         return self.img
 
 
-    def mandelbrot(self, c, z = None):
-        if z == None:
-            z = self.c
-        else:
-            z = (z * z) + c
+    def mandelbrot(self, c):
+        z = 0j
         n_iter = 0    # Number of iterations to escape
-        while n_iter < self.iteration_depth:
-            if z.real > 2.0:    # Escape condition
-                return n_iter
-            else:
-                z = (z * z) + c
+        while n_iter < self.iteration_depth and z.real * z.real + z.imag * z.imag < 2.0 * 2.0 and not math.isnan(z.real) and not  math.isnan(z.imag):
+            z = (z * z) + c
             n_iter += 1
         return n_iter
 
@@ -79,3 +74,4 @@ class Mandelbrot():
         t11 = time.time()
         print("Gif created, time taken: ", str(t11 - t00))
         img.save(path, save_all = True, append_images = [i for i in images], loop = 0, duration = 250)
+
