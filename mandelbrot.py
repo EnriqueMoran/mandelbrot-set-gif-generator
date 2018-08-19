9052d5cc256a9fc710692bda8f58955815552755
Enrique Moran <enriquemoran95@gmail.com>
2018-08-19 15:48:54 +0200

    Threading

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,21 +1,16 @@
 import time
 import math
 import random
-import multiprocessing
 from multiprocess.pool import Pool
 from ctypes import windll
 from PIL import Image
 
-#t_img = Image.new("HSV", (self.image_width, self.image_height))
 
 class Mandelbrot():
 
     __author__ : "EnriqueMoran"
 
     __version__ : "1.2"
-
-    #def transformInterval(self, e1, e2, s1, s2, x):    # Transform [0, image_width] and [0, image_height] into [-2, 2] interval
-     #   return (s1 - s2) / (e1 - e2) * (x - e1) + s1
 
     def __init__(self, image_width, image_height, image_path, iteration_depth = 100, zoom = 1, x0 = 0, y0 = 0):
         self.image_width = image_width
@@ -27,12 +22,9 @@ class Mandelbrot():
         self.s2_x = x0 + 2 * zoom
         self.s2_y = y0 + 2 * -zoom  # Initial y coordinate
         self.s1_y = y0 + 2 * zoom
-        self.img = Image.new("HSV", (self.image_width, self.image_height))
-        self.pixels = self.img.load()
 
 
     def drawFractal(self, x0, y0, x1, y1):
-
         image_width = self.image_width
         image_height = self.image_height
         s1_x = self.s1_x
@@ -59,39 +51,32 @@ class Mandelbrot():
             return n & 255, (n >> 8) & 255, (n >> 16) & 255
 
         def draw(x0, y0, x1, y1):
-
-            t_img = Image.new("HSV", (image_width, image_height))
+            t_img = Image.new("HSV", (int(x1) - int(x0), int(y1) - int(y0)))
             pixels = t_img.load()
-
             for i in range(int(x0), int(x1)):
                 for j in range(int(y0), int(y1)):
                     val_i = transformInterval(0, image_width, s1_x, s2_x, i)    # Normalize i and j between [-2, 2]
                     val_j = transformInterval(0, image_height, s1_y, s2_y, j)
                     value = mandelbrot(complex(val_i, val_j))
                     if value == iteration_depth:
-                        pixels[i, j] = (int(value), 255 , 0)
+                        pixels[i % (int(x1) - int(x0)), j % (int(y1) - int(y0))] = (int(value), 255 , 0)
                     else:
-                        #blue, green, red = intToRGB(value)    # Black and red image
-                        #pixels[i, j] = (red, green, blue)
-                        pixels[i, j] = (int(value), 255, 255)
+                        pixels[i % (int(x1) - int(x0)), j % (int(y1) - int(y0))] = (int(value), 255, 255)
             return t_img
 
-
-        with Pool(processes = 4) as pool:
-            p1, p2, p3, p4 = pool.starmap(draw, [(0, 0, image_width / 2, image_height / 2), (image_width / 2, 0, image_width, image_height / 2), (0, image_height / 2, image_width / 2, image_height), (image_width / 2, image_height / 2, image_width, image_height)])
+        #with Pool(processes = 4) as pool:
+        #    p1, p2, p3, p4 = pool.starmap(draw, [(0, 0, image_width / 2, image_height / 2), (image_width / 2, 0, image_width, image_height / 2), (0, image_height / 2, image_width / 2, image_height), (image_width / 2, image_height / 2, image_width, image_height)])
+        with Pool(processes = 1) as pool:
+            p1 = pool.starmap(draw, [(0, 0, image_width / 2, image_height / 2)])
             pool.close()
             pool.join()
             img = Image.new("HSV", (image_width, image_height))
-            crop_p1 = p1.crop((0, 0, int(image_width / 2), int(image_height / 2)))
-            img.paste(crop_p1, (0, 0))
-            crop_p2 = p2.crop((int(image_width / 2), 0, image_width, int(image_height / 2)))
-            img.paste(crop_p2, (int(image_width / 2), 0))
-            crop_p3 = p3.crop((0, int(image_height / 2), int(image_width / 2), image_height))
-            img.paste(crop_p3, (0, int(image_height / 2)))
-            crop_p4 = p4.crop((int(image_width / 2), int(image_height / 2), image_width, image_height))
-            img.paste(crop_p4, ((int(image_width / 2), int(image_height / 2))))
+            #img.paste(p1, (0, 0))
+            #img.paste(p2, (int(image_width / 2), 0))
+            #img.paste(p3, (0, int(image_height / 2)))
+            #img.paste(p4, ((int(image_width / 2), int(image_height / 2))))
 
-        return img
+        return p1
 
 
     def createGif(self, x, y, n_frames, initial_zoom, zoom_per_frame, path, iterations, save_frames = False):
@@ -103,17 +88,10 @@ class Mandelbrot():
             mand = Mandelbrot(self.image_width, self.image_height, path, iterations, zoom, x, y)
             t_img = mand.drawFractal(0, 0, self.image_width, self.image_height)
 
-            #t_img = Image.new("HSV", (self.image_width, self.image_height))
-            #px = t_img.load()
-            #for i in range(self.image_width):
-            #    for j in range(self.image_height):
-            #        px[i, j] = new_pixels[i, j]
             images.append(t_img)
             t1 = time.time()
             print("Frame number ", str(i + 1), " generated. Time taken: ", str(t1 - t0))
             zoom -= 4 * zoom / zoom_per_frame
-
-
 
         t11 = time.time()
         print("Gif created, time taken: ", str(t11 - t00))
@@ -127,7 +105,7 @@ if __name__ == "__main__":
     iterations = 500
     image_width = 500
     image_heigth = 500
-    n_frames = 5
+    n_frames = 1
     frames_per_zoom = 10
     image_zoom = 1
     x_coordinate = 0
